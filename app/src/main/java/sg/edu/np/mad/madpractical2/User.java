dcc8e790137d432b73e21d7f79883f4cfc10f0ce
iamalolz <fanboiv2@gmail.com>
2024-04-25 13:49:09 +0800

    nth commit

--- app/src/main/java/sg/edu/np/mad/madpractical2/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/User.java
@@ -5,4 +5,12 @@ public class User {
     public String description;
     public int id;
     public boolean followed;
+
+    public User(String name, String description, int id, boolean followed) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.followed = followed;
+    }
 }
+
