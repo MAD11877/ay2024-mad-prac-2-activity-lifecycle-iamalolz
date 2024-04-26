5793eda8c237156adb624d2be1b67f3c4a89898b
iamalolz <fanboiv2@gmail.com>
2024-04-26 14:50:06 +0800

    completed challenge

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         User user = new User("John Doe", "MAD Developer", 1, false);
-        TextView tvName = findViewById(R.id.tvDescription);
-        TextView tvDescription = findViewById(R.id.textView4);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        TextView tvName = findViewById(R.id.tvName);
         Button btnFollow = findViewById(R.id.btnFollow);
         tvName.setText(user.name);
         tvDescription.setText(user.description);
         btnFollow.setText("Follow");
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.followed) {
+                    btnFollow.setText("Follow");
+                    user.followed = false;
+                } else {
+                    btnFollow.setText("Unfollow");
+                    user.followed = true;
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
